84fcbba63297982c9c4a6c5f7424f6cfcabe671d
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-23 13:45:43 +0200

    update on service configuration

--- app/src/main/java/be/hcpl/android/sensors/ScheduleServiceFragment.java
+++ app/src/main/java/be/hcpl/android/sensors/ScheduleServiceFragment.java
@@ -8,13 +8,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import be.hcpl.android.sensors.core.BaseFragment;
 import be.hcpl.android.sensors.service.SensorBackgroundService;
 
 
 /**
- *
+ * A Fragment for managing the background service
  */
 public class ScheduleServiceFragment extends BaseFragment {
 
@@ -24,11 +26,10 @@ public class ScheduleServiceFragment extends BaseFragment {
      *
      * @return A new instance of fragment ScheduleServiceFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ScheduleServiceFragment newInstance() {
         ScheduleServiceFragment fragment = new ScheduleServiceFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
+        //Bundle args = new Bundle();
+        //fragment.setArguments(args);
         return fragment;
     }
 
@@ -54,16 +55,50 @@ public class ScheduleServiceFragment extends BaseFragment {
 
         // TODO add current state of service
 
-        // TODO add configuration options for service (interval, treshold, sensor, precision, ...)
+        // add configuration options for service (interval, treshold, sensor, precision, ...)
+        final EditText editMin = (EditText)view.findViewById(R.id.editMin);
+        final EditText editMax = (EditText)view.findViewById(R.id.editMax);
+        final EditText editInterval = (EditText)view.findViewById(R.id.editInterval);
+        final CheckBox chkLogging = (CheckBox)view.findViewById(R.id.chkLogging);
 
         view.findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // get scheduler and prepare intent
                 AlarmManager scheduler = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
                 Intent intent = new Intent(getActivity().getApplicationContext(), SensorBackgroundService.class);
+
+                // add some extras for config
+                Bundle args = new Bundle();
+                try {
+                    float value = Float.parseFloat(editMin.getText().toString());
+                    args.putFloat(SensorBackgroundService.KEY_THRESHOLD_MIN_VALUE, value);
+                }catch(Exception e){
+                    // ignore
+                }
+                try {
+                    float value = Float.parseFloat(editMax.getText().toString());
+                    args.putFloat(SensorBackgroundService.KEY_THRESHOLD_MAX_VALUE, value);
+                }catch(Exception e){
+                    // ignore
+                }
+                args.putBoolean(SensorBackgroundService.KEY_LOGGING, chkLogging.isChecked());
+                intent.putExtras(args);
+
+                // try getting interval option
+                long interval;
+                try{
+                    interval = Long.parseLong(editInterval.getText().toString());
+                }catch(Exception e){
+                    // use the default in that case
+                    interval = 1000L;
+                }
+
                 PendingIntent scheduledIntent = PendingIntent.getService(getActivity().getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                scheduler.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000, scheduledIntent);
+                // start the service
+                scheduler.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, scheduledIntent);
             }
         });
 
--- app/src/main/java/be/hcpl/android/sensors/service/SensorBackgroundService.java
+++ app/src/main/java/be/hcpl/android/sensors/service/SensorBackgroundService.java
@@ -7,33 +7,85 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
 
 /**
  * for a background service not linked to an activity it's important to use the command approach
- * instead of the Binder. For starting use the alarmmanager
+ * instead of the Binder. For starting use the alarm manager
  */
 public class SensorBackgroundService extends Service implements SensorEventListener {
 
+    /**
+     * a tag for logging
+     */
     private static final String TAG = SensorBackgroundService.class.getSimpleName();
 
-    private SensorManager sensorManager = null;
-    private Sensor sensor = null;
+    /**
+     * again we need the sensor manager and sensor reference
+     */
+    private SensorManager mSensorManager = null;
+
+    /**
+     * an optional flag for logging
+     */
+    private boolean mLogging = false;
+
+    /**
+     * an interval for the sensor value reading;
+     */
+    private long mInterval;
+
+    /**
+     * treshold values
+     */
+    private float mThresholdMin, mThresholdMax;
+
+    public static final String KEY_SENSOR_TYPE = "sensor_type";
+
+    public static final String KEY_THRESHOLD_MIN_VALUE = "threshold_min_value";
+
+    public static final String KEY_THRESHOLD_MAX_VALUE = "threshold_max_value";
+
+    public static final String KEY_LOGGING = "logging";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // get sensor manager on starting the service
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // have a default sensor configured
+        int sensorType = Sensor.TYPE_LIGHT;
+
+        Bundle args = intent.getExtras();
+
+        // get some properties from the intent
+        if (args != null) {
+
+            // set sensortype from bundle
+            if (args.containsKey(KEY_SENSOR_TYPE))
+                sensorType = args.getInt(KEY_SENSOR_TYPE);
+
+            // optional logging
+            mLogging = args.getBoolean(KEY_LOGGING);
+
+            // treshold values
+            // since we want to take them into account only when configured use min and max
+            // values for the type to disable
+            mThresholdMin = args.containsKey(KEY_THRESHOLD_MIN_VALUE) ? args.getFloat(KEY_THRESHOLD_MIN_VALUE) : Float.MIN_VALUE;
+            mThresholdMax = args.containsKey(KEY_THRESHOLD_MAX_VALUE) ? args.getFloat(KEY_THRESHOLD_MAX_VALUE) : Float.MAX_VALUE;
+        }
 
         // we need the light sensor
-        // TODO make this all configurable
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        Sensor sensor = mSensorManager.getDefaultSensor(sensorType);
 
-        sensorManager.registerListener(this, sensor,
+        // TODO we could have the sensor reading delay configurable also though that won't do much
+        // in this use case since we work with the alarm manager
+        mSensorManager.registerListener(this, sensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
-
 
         return START_STICKY;
     }
@@ -56,38 +108,44 @@ public class SensorBackgroundService extends Service implements SensorEventListe
         // for recording of data use an AsyncTask, we just need to compare some values so no
         // background stuff needed for this
 
-        // grab the values
-        StringBuilder sb = new StringBuilder();
-        for (float value : event.values )
-            sb.append(String.valueOf(value)).append(" | ");
-
         // Log that information for so we can track it in the console (for production code remove
         // this since this will take a lot of resources!!)
-        Log.d(TAG, "received sensor valures are: " + sb.toString());
+        if (mLogging) {
 
-        // if first value is below 10 (TODO make treshold configurable)
+            // grab the values
+            StringBuilder sb = new StringBuilder();
+            for (float value : event.values)
+                sb.append(String.valueOf(value)).append(" | ");
+
+            Log.d(TAG, "received sensor valures are: " + sb.toString());
+        }
+
+        // get the value
+        // TODO we could make the value index also configurable, make it simple for now
+        float sensorValue = event.values[0];
+
+        // if first value is below min or above max threshold but only when configured
         // we need to enable the screen
-        if( event.values[0] < 10 ){
+        if (sensorValue < mThresholdMin || sensorValue > mThresholdMax) {
 
             // TODO fix deprecated code here
+            // TODO we could even make the actions configurable...
 
             // wake screen here
             PowerManager pm = (PowerManager) getApplicationContext().getSystemService(getApplicationContext().POWER_SERVICE);
-            PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+            PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), TAG);
             wakeLock.acquire();
 
             // and release screen lock right away also
             KeyguardManager keyguardManager = (KeyguardManager) getApplicationContext().getSystemService(getApplicationContext().KEYGUARD_SERVICE);
-            KeyguardManager.KeyguardLock keyguardLock =  keyguardManager.newKeyguardLock("TAG");
+            KeyguardManager.KeyguardLock keyguardLock = keyguardManager.newKeyguardLock(TAG);
             keyguardLock.disableKeyguard();
         }
 
         // stop the sensor and service
-        sensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this);
         stopSelf();
     }
-
-
 
 
 }
