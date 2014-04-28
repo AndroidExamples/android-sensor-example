18b3ec0e88dfc963c0f15640b411466635ab522e
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-28 11:26:51 +0200

    removed unused code from MainActivity

--- app/src/main/java/be/hcpl/android/sensors/MainActivity.java
+++ app/src/main/java/be/hcpl/android/sensors/MainActivity.java
@@ -68,22 +68,6 @@ public class MainActivity extends ActionBarActivity
         mContentFragment = fragment;
     }
 
-    /*
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-    */
-
     public void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
